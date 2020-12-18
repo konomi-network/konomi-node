07a732738c157549c559e242a3f0f97ca253f8b2
TinyRollup <john@konomi.network>
2020-12-19 07:53:25 +0800

    add accrue interest

--- pallets/lending/src/lib.rs
+++ pallets/lending/src/lib.rs
@@ -1,23 +1,24 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::{
-    decl_event, decl_module, decl_storage, Parameter,
+    decl_event, decl_module, decl_storage,
     StorageMap, StorageValue,
 };
 use sp_runtime::{DispatchResult as Result, RuntimeDebug};
 use frame_system::{self as system, ensure_signed};
-use sp_core::crypto::{UncheckedFrom, UncheckedInto};
 use sp_std::prelude::*;
-use sp_std::{marker::PhantomData, mem, vec::Vec};
-use sp_runtime::traits::{
-    Bounded, Hash, AtLeast32BitUnsigned, Zero,
-};
+use sp_std::{vec::Vec, convert::TryInto};
 use pallet_assets as assets;
 use codec::{Encode, Decode};
+use substrate_fixed::{
+    types::U64F64,
+    traits::FromFixed
+};
 
 // TODO: fee, reserves
 // TODO: loose couple
 // TODO: child storage
+// TODO: U64F64 as type
 
 /// The module's configuration trait.
 pub trait Trait: assets::Trait {
@@ -39,15 +40,15 @@ pub struct Pool<T: Trait> {
 
     pub debt: T::Balance,
 
-    pub liquidation_threshold: T::Balance,
+    pub liquidation_threshold: U64F64,
 
-    pub supply_threshold: T::Balance,
+    pub supply_threshold: U64F64,
 
-    pub liquidation_bonus: T::Balance,
+    pub liquidation_bonus: U64F64,
 
-    pub total_supply_index: u64,
+    pub total_supply_index: U64F64,
 
-    pub total_debt_index: u64,
+    pub total_debt_index: U64F64,
 
     pub last_updated: T::BlockNumber // TODO: considering timestamp?
     
@@ -98,6 +99,8 @@ decl_storage! {
 
         pub UserCollaterals: map hasher(blake2_128_concat) T::AccountId => Vec<T::AssetId>;
 
+        Tether get(fn test): u32;
+
     }
 
 }
@@ -110,7 +113,7 @@ decl_module! {
         // this is needed only if you are using events in your module
         fn deposit_event() = default;
 
-        /// end user related
+        // end user related
  
         #[weight = 1]
         fn supply(
@@ -119,8 +122,9 @@ decl_module! {
             amount: T::Balance) -> Result {
             let account = ensure_signed(origin)?;
 
-            // TODO
+            // TODO check pool exists
             // 1 accrue interest
+            Self::accrue_interest(asset_id);
             // 2 transfer asset
             // 3 update user supply
             // 4 update pool supply
@@ -135,8 +139,9 @@ decl_module! {
             amount: T::Balance) -> Result {
             let account = ensure_signed(origin)?;
 
-            // TODO
+            // TODO check pool exists
             // 1 accrue interest
+            Self::accrue_interest(asset_id);
             // 2 check collateral and pool cash = (deposit - borrow)
             // 3 update user supply
             // 4 transfer asset to user
@@ -151,8 +156,9 @@ decl_module! {
             amount: T::Balance) -> Result {
             let account = ensure_signed(origin)?;
 
-            // TODO
+            // TODO check pool exists
             // 1 accrue interest
+            Self::accrue_interest(asset_id);
             // 2 check collateral
             // 3 update user Borrow
             // 4 update pool borrow
@@ -167,8 +173,9 @@ decl_module! {
             amount: T::Balance) -> Result {
             let account = ensure_signed(origin)?;
 
-            // TODO
+            // TODO check pool exists
             // 1 accrue interest
+            Self::accrue_interest(asset_id);
             // 2 transfer token from user
             // 3 update user borrow: if all loan is repaid, clean up the loan
             // 4 update pool borrow
@@ -179,7 +186,7 @@ decl_module! {
         #[weight = 1]
         fn choose_collateral(
             origin,
-            asset_id: t::AssetId,
+            asset_id: T::AssetId,
             as_collateral: bool
         ) -> Result {
 
@@ -187,7 +194,7 @@ decl_module! {
             Ok(())
         }
 
-        /// arbitrager related
+        // arbitrager related
 
         #[weight = 1]
         fn liquidate(
@@ -199,9 +206,11 @@ decl_module! {
         ) -> Result {
             let account = ensure_signed(origin)?;
 
-            // TODO
+            // TODO check pool exists
             // 1 check if get_asset_id is enabled as collatoral
             // 2 accrue interest of pay and get asset
+            Self::accrue_interest(pay_asset_id);
+            Self::accrue_interest(get_asset_id);
             // 3 check if target user is under liquidation condition
             // 4 check if liquidation % is more than threshold 
             // 5 transfer token from arbitrager
@@ -211,14 +220,14 @@ decl_module! {
             Ok(())
         }
         
-        /// governance related
+        // governance related
 
         #[weight = 1]
         fn init_pool(
             origin,
             id: T::AssetId,
             can_be_collateral: bool
-        ) -Result {
+        ) -> Result {
 
             Ok(())
         }
@@ -228,20 +237,55 @@ decl_module! {
 impl<T: Trait> Module<T>
 {
     fn accrue_interest(asset_id: T::AssetId) {
-        // TODO
+        // TODO avoid multi get pool
+        // TODO use error of convert error
         // 1 get pool
-        // 2 get supply/borrow rate
+        let mut pool = Self::pool(asset_id).unwrap();
+        if pool.last_updated == <frame_system::Module<T>>::block_number() {
+            return
+        }
+        // dealing with option
+
         // 3 get time span
-        // 4 calculate interest
-        // 5 update pool index, supply, borrow, timestamp
+        let interval_block_number = <frame_system::Module<T>>::block_number() - pool.last_updated;
+		let elapsed_time_u32 = TryInto::try_into(interval_block_number)
+			.ok()
+			.expect("blockchain will not exceed 2^32 blocks; qed");
+        let elapsed_time_U64F64 = U64F64::from_num(elapsed_time_u32);
+
+        // 4  get rates and calculate interest
+        let new_total_supply_index = Self::get_supply_rate(asset_id) * elapsed_time_U64F64 * pool.total_supply_index;
+        let new_total_debt_index = Self::get_debt_rate(asset_id) * elapsed_time_U64F64 * pool.total_debt_index;
+
+        // 5 update pool index, supply, debt, timestamp
+        let supply = TryInto::<u128>::try_into(pool.supply)
+            .ok()
+            .expect("Balance is u128");
+        let supply = supply * new_total_supply_index;
+        let converted = u128::from_fixed(supply);
+        pool.supply = TryInto::<T::Balance>::try_into(converted)
+            .ok()
+            .expect("Balance is u128");
+
+        let debt = TryInto::<u128>::try_into(pool.debt)
+            .ok()
+            .expect("Balance is u128");
+        let debt = debt * new_total_supply_index;
+        let converted = u128::from_fixed(debt);
+        pool.debt = TryInto::<T::Balance>::try_into(converted)
+            .ok()
+            .expect("Balance is u128");
+
+        Pools::<T>::insert(asset_id, pool);
+
     }
 
-    fn get_borrow_rate(asset_id: T::AssetId) -> T::Balance {
-        T::Balance::default()
+    fn get_debt_rate(asset_id: T::AssetId) -> U64F64 {
+        U64F64::from_num(1)
     }
 
-    fn get_supply_rate(asset_id: T::AssetId) -> T::Balance {
-        T::Balance::default()
+    fn get_supply_rate(asset_id: T::AssetId) -> U64F64 {
+        U64F64::from_num(1)
     }
 
     fn get_user_total_collaterals(account: T::AccountId) -> T::Balance {
